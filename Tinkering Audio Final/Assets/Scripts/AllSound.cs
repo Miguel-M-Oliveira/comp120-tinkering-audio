f62545502044d0f77bae9f014ffc6e52cbf98c9b
Miguel-M-Oliveira <miguel.oliveira2000@hotmail.com>
2019-11-29 15:45:04 +0000

    Added final version of program

---
+++ Tinkering Audio Final/Assets/Scripts/AllSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllSound : MonoBehaviour
+{
+    public int startingPitch = 1;
+    public int startingVolume = 1;
+    AudioSource audioSource;
+
+
+    public void Pitch(float value)
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = startingPitch;
+        audioSource.pitch = value;
+    }
+
+    public void Volume(float value)
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = startingVolume;
+        audioSource.volume = value;
+    }
+}
---
+++ Tinkering Audio Final/Assets/Scripts/Main_menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public void PlayGame ()
+        //Makes the "PLAY" button in the main menu load the "Game" scene
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+        //Makes the "QUIT" button in the main menu quit the game and type "QUIT" in the console.
+    {
+        Debug.Log("QUIT");
+        Application.Quit();     
+    }
+}

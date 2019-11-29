ef604d44e2a4a5544c7e01b58bdc3e0c98915114
Miguel-M-Oliveira <miguel.oliveira2000@hotmail.com>
2019-11-29 16:04:09 +0000

    Added more comments to scripts

--- Tinkering Audio Final/Assets/Scripts/AllSound.cs
+++ Tinkering Audio Final/Assets/Scripts/AllSound.cs
@@ -7,6 +7,7 @@ public class AllSound : MonoBehaviour
     public int startingPitch = 1;
     public int startingVolume = 1;
     AudioSource audioSource;
+    //Sets the starting pitch and volume to 1 and defines the variable "audioSource" as an AudioSource
 
 
     public void Pitch(float value)
@@ -14,6 +15,7 @@ public class AllSound : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.pitch = startingPitch;
         audioSource.pitch = value;
+        //Fetches the respective AudioSource component from the GameObject, and makes it so that the pitch changes in accordance to the "value", which is the slider.
     }
 
     public void Volume(float value)
@@ -21,5 +23,6 @@ public class AllSound : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = startingVolume;
         audioSource.volume = value;
+        //Fetches the respective AudioSource component from the GameObject, and makes it so that the volume changes in accordance to the "value", which is the slider.
     }
 }
